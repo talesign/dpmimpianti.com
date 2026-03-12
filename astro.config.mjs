d08fb573006454325a8b0e9ca717d298aa24235e
Oliviero Taleghani <o.taleghani@talesign.com>
2026-03-12 15:00:55 +0100

    added: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,9 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.dpmimpianti.com",
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [react(), sitemap()],
 });
-
